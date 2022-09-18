59bdd644e327af5510f947a151ebbff250416d24
Mr.Smoke <kumroshan120@gmail.com>
2022-09-18 13:54:09 +0530

    The Quest for Perfection

--- Fundamentals/PerfectNumber.java
+++ Fundamentals/PerfectNumber.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 public class PerfectNumber {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    try (Scanner sc = new Scanner(System.in)) {
+      int test = sc.nextInt();
 
-    int test = sc.nextInt();
+      while (test-- > 0) {
+        int n = sc.nextInt();
+        n = (int) Math.sqrt(n);
+        int sum = 0, x = 2;
 
-    while (test-- > 0) {
-      int n = sc.nextInt();
-      n = (int) Math.sqrt(n);
-      int sum = 0, x = 2;
-
-      while (x <= n) {
-        if (n % x == 0) {
-          sum += x;
-          x++;
+        while (x <= n) {
+          if (n % x == 0) {
+            sum += x;
+            x++;
+          }
         }
-      }
-      if (sum == n) {
-        System.out.println("Yes");
-      } else {
-        System.out.println("No");
+        if (sum == n) {
+          System.out.println("Yes");
+        } else {
+          System.out.println("No");
+        }
       }
     }
   }
